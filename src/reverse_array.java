46767a7f81ce77abaa6ef523a89024ab579894e8
saloni <saloni.bindra2014@gmail.com>
2017-01-13 21:11:05 -0500

    added java class Rev-Array

---
+++ src/reverse_array.java
@@ -0,0 +1,116 @@
+import java.util.*;
+/**
+ * Created by salonibindra on 1/9/17.
+ */
+public class reverse_array {
+
+    public static void main(String[] args) {
+        System.out.println("hry");
+        reverse_array r = new reverse_array();
+        //r.disp();
+        r.rotate_right();
+    }
+
+    public void disp()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string you want to reverse :");
+        String s = sc.nextLine();
+        System.out.println(s);
+
+
+    }
+    public void rotate_right()
+    {   Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the  number of elements in array");
+        int n = sc.nextInt();
+        int a[] = new int [n];
+
+
+        System.out.println("Enter the elements");
+        for (int i = 0; i < n; i++) {
+            a[i]=sc.nextInt();
+
+        }
+
+        System.out.println("Enter the number of elements to be rotated");
+        int k = sc.nextInt();
+
+        int temp[] = new int [k];
+
+        for (int i = 0,j=n-1; i <k ; i++,j--) {
+            temp[i]=a[j];
+            System.out.print(temp[i]+" ");
+
+        }
+        System.out.println();
+
+        for (int i = n-k-1, j=n-1; i >=0; i--,j--) {
+            a[j]=a[i];
+            System.out.print(a[i]+" "+j);
+
+        }
+        System.out.println();
+
+        for (int i = 0, j=k-1; i <k ; i++,j--) {
+            a[i]=temp[j];
+            System.out.print(a[i]+" is at "+i+"  ");
+
+        }
+        System.out.println();
+
+        for (int i = 0; i <n ; i++) {
+            System.out.print(a[i]+"  ");
+
+        }
+    }
+
+    public void rotate_left()
+    {   Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the  number of elements in array");
+        int n = sc.nextInt();
+        int a[] = new int [n];
+
+
+        System.out.println("Enter the elements");
+        for (int i = 0; i < n; i++) {
+            a[i]=sc.nextInt();
+
+        }
+
+        System.out.println("Enter the number of elements to be rotated");
+        int k = sc.nextInt();
+
+        int temp[] = new int [k];
+
+        for (int i = 0,j=0; i <k ; i++,j++) {
+            temp[i]=a[j];
+            System.out.print(temp[i]+" ");
+
+        }
+        System.out.println();
+
+        for (int i = n-k-1, j=n-1; i >=0; i--,j--) {
+            a[j]=a[i];
+            System.out.print(a[i]+" "+j);
+
+        }
+        System.out.println();
+
+        for (int i = 0, j=k-1; i <k ; i++,j--) {
+            a[i]=temp[j];
+            System.out.print(a[i]+" is at "+i+"  ");
+
+        }
+        System.out.println();
+
+        for (int i = 0; i <n ; i++) {
+            System.out.print(a[i]+"  ");
+
+        }
+
+    }}
+
