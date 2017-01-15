868f4ed50ac9f263d7c9a1e01d906ff2ed0c8202
saloni <saloni.bindra2014@gmail.com>
2017-01-15 10:24:05 -0500

    Added Permuation Scenario

---
+++ src/CheckPermutation.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+/**
+ * Created by salonibindra on 1/14/17.
+ */
+public class CheckPermutation {
+    public static void main(String[] args) {
+        String a = "qwert";
+        String b = "terwq";
+
+        CheckPermutation ob = new CheckPermutation();
+        System.out.println(ob.Validate(a,b));
+    }
+
+    public boolean Validate (String a , String b)
+    {
+        if (a.length()!=b.length())
+        {
+            System.out.println("length");return false;
+        }
+
+        char[] x1 = a.toCharArray();
+        char[] x2 = b.toCharArray();
+
+        Arrays.sort(x1);
+        String a1= new String(x1);
+        System.out.println(x1);
+
+        Arrays.sort(x2);
+        String b2 = new String(x2);
+        System.out.println(x2);
+        return (a1.equals(b2));
+
+    }
+}
